870d94d01dce9a379acda40311b21cb32872da77
Bezalyel Silva <bezalyelsilva@gmail.com>
2023-12-28 20:13:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 public class Main {
-    
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-        int opcao_menu = 0;
-        while (opcao_menu == 0) {
-            System.out.println("BEM VINDO AO MENU DO CAMPEONATO\nEscolha uma opcao abaixo");           
-    System.out.println("\n1 - Tabela de Classificacao\n2 - Tabela de Artilheiros\n3 - Tabela de Jogos");
-    opcao_menu = scanner.nextInt();
-            if (opcao_menu == 1) {
-                System.out.println("Tabela de Classificacao:\n1 - Brasil 40 pontos\n2 - Argentina 30 pontos\n3 - Espanha 20 pontos\n4 - Uruguai 10 pontos\n5 - França 5 pontos");
-                System.out.println("Digite 0 para voltar ao Menu Inicial");
-                opcao_menu = scanner.nextInt();
-            } else if (opcao_menu == 2) {
-                System.out.println("Tabela de Melhores Marcadores\n1 - Neymar jr 99 gols\n2 - Cristiano Ronaldo 98 gols\n3 - Messi 92 gols\n4 - Mbappe 79 gols\n5 - Vinicius Jr 78 gols\n6 - Rodrygo 75 gols");
-                System.out.println("Digite 0 para voltar ao Menu inicial");
-                opcao_menu = scanner.nextInt();
-            } else if (opcao_menu == 3) {
-                System.out.println("Horario dos Jogos\n1 - Brasil X Espanha 14:30h\n2 - Argentina X França 18:30h");
-                System.out.println("Digite 0 para voltar ao Menu inicial");
-                opcao_menu = scanner.nextInt();
-            }
-            
-            else if (opcao_menu == 0) {
+        Scanner scanner = new Scanner(System.in);
+        int menu_option = 0;
+        
+        while (menu_option == 0) {
+            System.out.println("WELCOME TO THE CHAMPIONSHIP MENU\nChoose an option below");           
+            System.out.println("\n1 - Standings Table\n2 - Top Scorers Table\n3 - Games Schedule");
+            menu_option = scanner.nextInt();
+    
+            if (menu_option == 1) {
+                System.out.println("Standings Table:\n1 - Brazil 40 points\n2 - Argentina 30 points\n3 - Spain 20 points\n4 - Uruguay 10 points\n5 - France 5 points");
+                System.out.println("Type 0 to go back to the Main Menu");
+                menu_option = scanner.nextInt();
+            } else if (menu_option == 2) {
+                System.out.println("Top Scorers Table\n1 - Neymar jr 99 goals\n2 - Cristiano Ronaldo 98 goals\n3 - Messi 92 goals\n4 - Mbappe 79 goals\n5 - Vinicius Jr 78 goals\n6 - Rodrygo 75 goals");
+                System.out.println("Type 0 to go back to the Main Menu");
+                menu_option = scanner.nextInt();
+            } else if (menu_option == 3) {
+                System.out.println("Game Schedule\n1 - Brazil vs Spain at 2:30 PM\n2 - Argentina vs France at 6:30 PM");
+                System.out.println("Type 0 to go back to the Main Menu");
+                menu_option = scanner.nextInt();
+            } else if (menu_option == 0) {
                 break;
             } else {
-                System.out.println("Opcao Invalida, Tente outro");
-                opcao_menu = scanner.nextInt();
+                System.out.println("Invalid Option, Try Another One");
+                menu_option = scanner.nextInt();
             }
         }
     }
 }
-
